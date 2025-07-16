89d9daadde00164750f55c713d2c9a1a16eba552
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-07-16 19:51:26 +0100

    Adds context for the coloured lights

--- src/machines/uk-traffic-light/uk-traffic-light.test.ts
+++ src/machines/uk-traffic-light/uk-traffic-light.test.ts
@@ -12,22 +12,46 @@ test("should transition correctly between all states", () => {
 
   // Test initial state
   expect(trafficLight.getSnapshot().matches("red")).toBe(true);
-
+  expect(trafficLight.getSnapshot().context.activeSignals).toEqual({
+    red: true,
+    amber: false,
+    green: false,
+  });
   // Test red -> red + amber
   trafficLight.send({ type: "timer" });
   expect(trafficLight.getSnapshot().matches("redAmber")).toBe(true);
+  expect(trafficLight.getSnapshot().context.activeSignals).toEqual({
+    red: true,
+    amber: true,
+    green: false,
+  });
 
   // Test red + amber -> green
   trafficLight.send({ type: "timer" });
   expect(trafficLight.getSnapshot().matches("green")).toBe(true);
+  expect(trafficLight.getSnapshot().context.activeSignals).toEqual({
+    red: false,
+    amber: false,
+    green: true,
+  });
 
   // Test green -> amber
   trafficLight.send({ type: "timer" });
   expect(trafficLight.getSnapshot().matches("amber")).toBe(true);
+  expect(trafficLight.getSnapshot().context.activeSignals).toEqual({
+    red: false,
+    amber: true,
+    green: false,
+  });
 
   // Test amber -> red
   trafficLight.send({ type: "timer" });
   expect(trafficLight.getSnapshot().matches("red")).toBe(true);
+  expect(trafficLight.getSnapshot().context.activeSignals).toEqual({
+    red: true,
+    amber: false,
+    green: false,
+  });
 
   // Clean up
   trafficLight.stop();
--- src/machines/uk-traffic-light/uk-traffic-light.ts
+++ src/machines/uk-traffic-light/uk-traffic-light.ts
@@ -6,30 +6,62 @@
 
 import { setup } from "xstate";
 
+export interface UkTrafficLightContext {
+  activeSignals: {
+    red: boolean;
+    amber: boolean;
+    green: boolean;
+  };
+}
+
+const activeSignals = {
+  red: false,
+  amber: false,
+  green: false,
+};
+
 export const ukTrafficLightMachine = setup({
   types: {
+    context: {} as UkTrafficLightContext,
     events: {} as { type: "timer" },
   },
 }).createMachine({
   id: "ukTrafficLight",
   initial: "red",
+  context: {
+    activeSignals,
+  },
   states: {
     red: {
+      entry: ({ context }) =>
+        (context.activeSignals = { ...activeSignals, red: true }),
       on: {
         timer: "redAmber",
       },
     },
     redAmber: {
+      entry: [
+        ({ context }) =>
+          (context.activeSignals = {
+            ...activeSignals,
+            red: true,
+            amber: true,
+          }),
+      ],
       on: {
         timer: "green",
       },
     },
     green: {
+      entry: ({ context }) =>
+        (context.activeSignals = { ...activeSignals, green: true }),
       on: {
         timer: "amber",
       },
     },
     amber: {
+      entry: ({ context }) =>
+        (context.activeSignals = { ...activeSignals, amber: true }),
       on: {
         timer: "red",
       },
